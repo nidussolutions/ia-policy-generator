358ebb7aea3a06c4fde7978ae16bb38e3384c300
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-04-16 12:22:45 -0300

    Feat: Adicionado prisma para comunicação com DB

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import { PrismaClient } from './generated/prisma';
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Servidor está funcionando!');
+app.get('/', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).send({ users });
 });
 
 app.listen(PORT, () => {
