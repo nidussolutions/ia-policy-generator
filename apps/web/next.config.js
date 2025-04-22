4a80c49a99012fc5753987eb744550ba9230c161
João Gustavo <jgbispo20@gmail.com>
2025-04-22 09:28:41 -0300

    Fixed bug in payment-confirmation

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {{experimental: {missingSuspenseWithCSRBailout: boolean}}} */
+const nextConfig = {
+    experimental:{
+        missingSuspenseWithCSRBailout: false,
+    }
+};
 
 export default nextConfig;
