e41078bf584320ac620e318973a21f8997b5f834
JG <jgbispo20@gmail.com>
2025-04-27 23:17:13 -0300

    add google tag

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -12,6 +12,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <head>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P99YBJN20E"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P99YBJN20E');
+</script>
+        </head>
       <body className={inter.className}>
         <I18nProvider>{children}</I18nProvider>
       </body>
