207d98b5223378c00f405ce1638f79fca98b6ebb
João Gustavo Bispo <jgbispo20@gmail.com>
2025-04-30 17:23:42 -0300

    Update index.ts

--- apps/server/src/routes/admin/index.ts
+++ apps/server/src/routes/admin/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import authRouter from "./authAdmin";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use('/auth', authRouter)
 
 export default app;
